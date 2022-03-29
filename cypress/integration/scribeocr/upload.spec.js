e23900b9d97326fa28165155d556f2804e018c9a
Andrew de Lamare <80968376+andrewdelamare@users.noreply.github.com>
2022-03-29 20:03:21 +0300

    add png+jocr & pdf+xml upload tests

--- cypress/integration/scribeocr/upload.spec.js
+++ cypress/integration/scribeocr/upload.spec.js
@@ -1,25 +1,35 @@
 //Please replace httpServer with the correct address for your testing server or an environment variable
 const httpServer = 'http://192.168.50.10:8080';
 
-describe('Does it upload', () => {
+describe('Upload', () => {
   beforeEach(() => {
     cy.visit(httpServer);
   })
   it('loads', () => {
     cy.get('#navbar').should('not.be.empty')
   })
-  it('a jpeg with hOCR?', () => {
-    // We use the `cy.get()` command to get all elements that match the selector.
-    // Then, we use `should` to assert that there are two matched items,
-    // which are the two default items.
+  it('a jpeg with hOCR', () => {
     cy.get('#nav-import-tab').click()
     cy.get('#uploader').selectFile(['cypress/fixtures/jpeg_hocr/snow_drops.hocr', 'cypress/fixtures/jpeg_hocr/snow_drops.jpg'])
-
-    // We can go even further and check that the default todos each contain
-    // the correct text. We use the `first` and `last` functions
-    // to get just the first and last matched elements individually,
-    // and then perform an assertion with `should`.
-    // cy.get('.todo-list li').first().should('have.text', 'Pay electric bill')
-    // cy.get('.todo-list li').last().should('have.text', 'Walk the dog')
+    cy.get('#nav-view-tab').click()
+    cy.get('#importProgress').should('be.visible')
+    
   })
+  it('a png with hOCR', () => {
+    cy.get('#nav-import-tab').click()
+    cy.get('#uploader').selectFile(['cypress/fixtures/png_hocr/pretty_faces.hocr', 'cypress/fixtures/png_hocr/pretty_faces.png'])
+    cy.get('#nav-view-tab').click()
+    cy.get('#importProgress').should('be.visible')
+  })
+  it('a pdf with xml', () => {
+    cy.get('#nav-import-tab').click()
+    cy.get('#uploader').selectFile(
+      ['cypress/fixtures/pdf_xml/poeticalworksofm02priouoft_abbyy.xml', 
+      'cypress/fixtures/pdf_xml/poeticalworksofm02priouoft_bw.pdf']
+    )
+    cy.get('#nav-view-tab').click()
+    cy.get('#nav-import-tab').click()
+    cy.get('#importProgress').should('be.visible')
+  })
+  
 })
