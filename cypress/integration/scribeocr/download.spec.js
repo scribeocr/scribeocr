38aa8e44d084ffeacdf52257525b582a287bf539
Andrew de Lamare <80968376+andrewdelamare@users.noreply.github.com>
2022-04-05 08:50:37 +0300

    moving things around for browserstack integration

--- cypress/integration/scribeocr/download.spec.js
+++ cypress/integration/scribeocr/download.spec.js
@@ -1,5 +1,6 @@
 //Please replace httpServer with the correct address for your testing server or an environment variable
-const httpServer = 'http://192.168.50.10:8080';
+//const httpServer = 'http://192.168.50.10:8080';
+const httpServer = 'https://scribeocr.com/';
 
 describe('Download', () => {
   beforeEach(() => {
@@ -8,7 +9,7 @@ describe('Download', () => {
   })
   it('text file from jpg with hOCR', () => {
     cy.get('#nav-import-tab').click()
-    cy.get('#uploader').selectFile(['cypress/fixtures/jpeg_hocr/snow_drops.hocr', 'cypress/fixtures/jpeg_hocr/snow_drops.jpg'])
+    cy.get('#uploader').selectFile(['cypress/fixtures/snow_drops.hocr', 'cypress/fixtures/snow_drops.jpg'])
     cy.get('#pageCount').should('have.text', '1')
     cy.wait(500)
     cy.get('#nav-download-tab').click()
@@ -33,7 +34,7 @@ describe('Download', () => {
   }) 
   */
   it('text file from a png with hOCR', () => {
-    cy.get('#uploader').selectFile(['cypress/fixtures/png_hocr/pretty_faces.hocr', 'cypress/fixtures/png_hocr/pretty_faces.png'])
+    cy.get('#uploader').selectFile(['cypress/fixtures/pretty_faces.hocr', 'cypress/fixtures/pretty_faces.png'])
     cy.get('#importProgress').should('be.visible')
     cy.get('#pageCount').should('have.text', '1')
     cy.get('#nav-download-tab').click()
@@ -56,8 +57,8 @@ describe('Download', () => {
   */
   it('text file from a pdf with xml', () => {
     cy.get('#uploader').selectFile(
-      ['cypress/fixtures/pdf_xml/siegeofcorinthpo00byrorich_abbyy.xml', 
-        'cypress/fixtures/pdf_xml/siegeofcorinthpo00byrorich_bw.pdf'
+      ['cypress/fixtures/siegeofcorinthpo00byrorich_abbyy.xml', 
+        'cypress/fixtures/siegeofcorinthpo00byrorich_bw.pdf'
     ])
     cy.get('#pageCount').should('have.text', '118')
     cy.wait(5000)
--- cypress/integration/scribeocr/upload.spec.js
+++ cypress/integration/scribeocr/upload.spec.js
@@ -1,5 +1,6 @@
 //Please replace httpServer with the correct address for your testing server or an environment variable
-const httpServer = 'http://192.168.50.10:8080';
+//const httpServer = 'http://192.168.50.10:8080';
+const httpServer = 'https://scribeocr.com/';
 
 describe('Upload', () => {
   beforeEach(() => {
@@ -10,22 +11,22 @@ describe('Upload', () => {
   })
   it('a jpeg with hOCR', () => {
     cy.get('#nav-import-tab').click()
-    cy.get('#uploader').selectFile(['cypress/fixtures/jpeg_hocr/snow_drops.hocr', 'cypress/fixtures/jpeg_hocr/snow_drops.jpg'])
+    cy.get('#uploader').selectFile(['cypress/fixtures/snow_drops.hocr', 'cypress/fixtures/snow_drops.jpg'])
     cy.get('#pageCount').should('have.text', '1')
     cy.get('#importProgress').should('be.visible')
     
   })
   it('a png with hOCR', () => {
     cy.get('#nav-import-tab').click()
-    cy.get('#uploader').selectFile(['cypress/fixtures/png_hocr/pretty_faces.hocr', 'cypress/fixtures/png_hocr/pretty_faces.png'])
+    cy.get('#uploader').selectFile(['cypress/fixtures/pretty_faces.hocr', 'cypress/fixtures/pretty_faces.png'])
     cy.get('#importProgress').should('be.visible')
     cy.get('#pageCount').should('have.text', '1')
   })
   it('a pdf with xml', () => {
     cy.get('#nav-import-tab').click()
     cy.get('#uploader').selectFile(
-      ['cypress/fixtures/pdf_xml/siegeofcorinthpo00byrorich_abbyy.xml', 
-      'cypress/fixtures/pdf_xml/siegeofcorinthpo00byrorich_bw.pdf']
+      ['cypress/fixtures/siegeofcorinthpo00byrorich_abbyy.xml', 
+      'cypress/fixtures/siegeofcorinthpo00byrorich_bw.pdf']
     )
     cy.wait(3000)
     cy.get('#pageCount').should('have.text', '118')
