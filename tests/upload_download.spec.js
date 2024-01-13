f01c213f615b852184f702244c82d8a6f061524e
Balearica <admin@scribeocr.com>
2024-01-12 18:37:32 -0800

    Updated automated tests

--- tests/upload_download.spec.js
+++ tests/upload_download.spec.js
@@ -1,113 +1,20 @@
-import { Builder, By, until } from 'selenium-webdriver';
-import {Options} from "selenium-webdriver/chrome.js";
+import { createDriver } from "./helpers.js";
 
 import path from 'path';
 import { fileURLToPath } from 'url';
 globalThis.__dirname = path.dirname(fileURLToPath(import.meta.url));
 const port = 3031;
 
-const options = new Options();
-options.addArguments('--remote-debugging-pipe')
-options.addArguments('--ignore-ssl-errors=yes')
-options.addArguments('--ignore-certificate-errors')
-
-class CustomSeleniumActions {
-    constructor(driver) {
-        this.driver = driver;
-    }
-
-    async uploadFiles(files) {
-
-        const filesAbs = files.map((x) => "https://scribeocr.com/tests/assets/" + x);
-        const jsStr = "fetchAndImportFiles([" + filesAbs.map((x) => "'" + x + "'").join(", ") + "])";
-
-        await this.driver.executeScript(jsStr);
-
-        // Wait for import progress bar to fill up
-        let progressBar = await this.driver.findElement(By.css('#import-progress-collapse .progress-bar'));
-
-        await this.driver.wait(async () => {
-            const maxValue = await progressBar.getAttribute('aria-valuemax');
-            const currentValue = await progressBar.getAttribute('aria-valuenow');
-            return currentValue === maxValue;
-        }, 10000, 'Progress bar did not reach maximum value in time');
-    } 
-
-
-
-
-    async downloadAllFormats() {
-        // Click on the 'Download' tab
-        await this.driver.findElement(By.id('nav-download-tab')).click();
-        const currentTab = await this.driver.getWindowHandle();
-
-        const downloadFormat = async (format) => {
-
-            // Select the requested format
-            await this.driver.findElement(By.id('downloadFormat')).click();
-            await this.driver.findElement(By.id(`formatLabelOption${format}`)).click();
-
-            // Click the "download" button
-            await this.driver.wait(until.elementIsEnabled(this.driver.findElement(By.id('download'))), 5000);
-            await this.driver.findElement(By.id('download')).click();
-    
-            // Wait for progress bar to fill up
-            const progressBarDownload = await this.driver.findElement(By.css('#generate-download-progress-collapse .progress-bar'));
-            const maxValueDownload = await progressBarDownload.getAttribute('aria-valuemax');
-            await this.driver.wait(async () => {
-                let currentValue = await progressBarDownload.getAttribute('aria-valuenow');
-                return currentValue === maxValueDownload;
-            }, 5000, 'Progress bar did not reach maximum value in time');
-
-            // Firefox opens .pdf downloads in a new tab, so we need to switch back to the original.
-            const browser = (await this.driver.getCapabilities()).getBrowserName();
-            if (browser == "firefox" && format == "PDF") {
-                await this.driver.sleep(500);
-                this.driver.switchTo().window(currentTab) 
-            }
-    
-        }
-
-        await downloadFormat("PDF");
-        await downloadFormat("HOCR");
-        await downloadFormat("Docx");
-        await downloadFormat("Text");
-
-    }
-
-}
-
-
-
 describe('Generate output files using images and uploaded ABBYY XML', function () {
     let driver;
     let customActions;
-    this.timeout(15000);
-    // const appURL = `http://127.0.0.1:3031/`;
-    //const appURL = `https://scribeocr.com/`;
-    const appURL = `http://172.18.0.2:3031/`;
+    this.timeout(10000);
+    const appURL = process.env.SELENIUM ? `http://172.18.0.2:${port}/` : `http://localhost:${port}/`;
 
     before(async function () {
-        //driver = await new Builder().forBrowser('chrome').setChromeOptions(options).build();
-        
-        // Microsoft uses a longer name for Edge
-        let browser = process.env.BROWSER;
-        if (browser == 'edge') {
-            browser = 'MicrosoftEdge';
-        }
-        // Connect to service specified in env variable or default to 'selenium'
-        const host = process.env.SELENIUM || 'selenium';
-        const server = `http://${host}:4444`;
-        driver = await new Builder()
-            .usingServer(server)
-            .forBrowser(browser)
-            .setChromeOptions(options)
-            .build();
 
-        // Implicit wait -- If an element is not immediately visible, wait up to 3 seconds for it to become visible before throwing an error.
-        await driver.manage().setTimeouts({ implicit: 3000, script: 60000 });
-        customActions = new CustomSeleniumActions(driver);
-        
+        ({driver, customActions} = await createDriver());
+
     });
 
     it('1 .pdf file', async function () {
