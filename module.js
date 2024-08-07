f58a4883e696e3cf939200f4222ed42522bd18d0
Balearica <admin@scribeocr.com>
2024-08-06 23:50:58 -0700

    Added missing file

---
+++ module.js
@@ -0,0 +1,40 @@
+import { clearData } from './js/clear.js';
+import { opt } from './js/containers/app.js';
+import { gs } from './js/containers/schedulerContainer.js';
+import { handleDownload } from './js/export/export.js';
+import { initGeneralScheduler } from './js/generalWorkerMain.js';
+import { importFilesAll } from './js/import/import.js';
+
+const init = async () => {
+  await initGeneralScheduler();
+  // await initTesseractInWorkers({});
+  // const resReadyFontAllRaw = gs.setFontAllRawReady();
+  // await loadBuiltInFontsRaw().then(() => resReadyFontAllRaw());
+};
+
+const setOptions = (options) => {
+  if (options && options.reflow !== undefined && options.reflow !== null) opt.reflow = options.reflow;
+  if (options && options.extractText !== undefined && options.extractText !== null) opt.extractText = options.extractText;
+};
+
+const importFiles = (files) => importFilesAll(files);
+
+/**
+ * Clears all document-specific data.
+ */
+const clear = async () => {
+  await clearData();
+};
+
+const terminate = async () => {
+  await clearData();
+  await gs.terminate();
+};
+
+const download = async (format, output) => {
+  await handleDownload(format, output);
+};
+
+export default {
+  clear, download, importFiles, init, setOptions, terminate,
+};
