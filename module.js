d810d8994f9a2e13aaeb24c85d2b1e642f22c5b4
Balearica <admin@scribeocr.com>
2024-08-09 13:17:28 -0700

    Updated module interface

--- module.js
+++ module.js
@@ -1,16 +1,37 @@
 import { clearData } from './js/clear.js';
 import { opt } from './js/containers/app.js';
+import { ImageCache } from './js/containers/imageContainer.js';
 import { gs } from './js/containers/schedulerContainer.js';
 import { df } from './js/debugGlobals.js';
 import { download, exportData } from './js/export/export.js';
-import { initGeneralScheduler } from './js/generalWorkerMain.js';
+import { loadBuiltInFontsRaw } from './js/fontContainerMain.js';
+import { initGeneralScheduler, initTesseractInWorkers } from './js/generalWorkerMain.js';
 import { importFilesAll } from './js/import/import.js';
 
-const init = async () => {
+/**
+ * Initialize the workers that handle most operations.
+ * @param {Object} params
+ * @param {boolean} [params.pdf=false] - Load PDF renderer.
+ * @param {boolean} [params.tesseract=false] - Load Tesseract.
+ * The PDF renderer and Tesseract are automatically loaded when needed.
+ * Therefore, the only reason to set `pdf` or `tesseract` to `true` is to pre-load them.
+ */
+const init = async ({
+  pdf = false,
+  tesseract = false,
+}) => {
+  const pdfPromise = pdf ? ImageCache.getMuPDFScheduler() : Promise.resolve();
+
   await initGeneralScheduler();
-  // await initTesseractInWorkers({});
-  // const resReadyFontAllRaw = gs.setFontAllRawReady();
-  // await loadBuiltInFontsRaw().then(() => resReadyFontAllRaw());
+
+  if (tesseract) {
+    await initTesseractInWorkers({});
+    // TODO: Font loading should potentially be its own option.
+    const resReadyFontAllRaw = gs.setFontAllRawReady();
+    await loadBuiltInFontsRaw().then(() => resReadyFontAllRaw());
+  }
+
+  await pdfPromise;
 };
 
 /**
